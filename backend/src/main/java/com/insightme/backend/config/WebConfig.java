b1d0bd1a0bc153f788c4209ffd9f19ff2557c06b
jeonghak.jo <challenge0110@naver.com>
2026-08-19 23:10:03 +0900

    배포 프론트 CORS 허용

--- backend/src/main/java/com/insightme/backend/config/WebConfig.java
+++ backend/src/main/java/com/insightme/backend/config/WebConfig.java
@@ -19,7 +19,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**") // 모든 API 경로 허용
-                .allowedOrigins("http://localhost:3000") // Next.js 개발 서버
+                .allowedOrigins("http://localhost:3000", // 로컬
+                                "https://insightme-ai.vercel.app" // 실제 배포된 프론트
+                        ) // Next.js 개발 서버
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP Method
                 .allowedHeaders("*") // 모든 Header 허용
                 .allowCredentials(true); // 쿠키 및 인증정보 허용
